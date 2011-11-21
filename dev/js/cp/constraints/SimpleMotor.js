c9afb44ec2e0f2328c401d2e4aaad8776a88292c
Murphy <moerphy@googlemail.com>
2011-11-21 21:30:42 +0100

    Implement SimpleMotor constraint
    Adds cp/constraints/SimpleMotor. Can be added to bodies like this: new SimpleMotor( bodyA, bodyB, angularVelocity ) see http://chipmunk-physics.net/release/ChipmunkLatest-Docs/#ConstraintTypes-cpSimpleMotor #2

---
+++ dev/js/cp/constraints/SimpleMotor.js
@@ -0,0 +1,67 @@
+define(['cp/constraints/Constraint', 'cp/constraints/util', 'cp/cpf'], function(Constraint, util, cpf){
+  "use strict";
+  
+  /**
+   * @param {cp/Body} a
+   * @param {cp/Body} b
+   * @param {Number} rate
+   */
+  var SimpleMotor = function(a, b, rate){
+    Constraint.call(this, a, b);
+    this.rate = rate;
+    this.jAcc = 0;
+  };
+  
+  SimpleMotor.prototype = util.extend( new Constraint(), {
+    
+    /**
+     * @param {Number} dt
+     */
+    preStep: function(dt){
+      var a = this.a;
+      var b = this.b;
+      
+      // calculate moment of inertia coefficient.
+      this.iSum = 1/(a.i_inv + b.i_inv);
+      // compute max impulse
+      this.jMax = this.maxForce * dt;
+    },
+    
+    /**
+     * @param {Number} dt_coef
+     */
+    applyCachedImpulse: function(dt_coef){
+      var a = this.a;
+      var b = this.b;
+      
+      var j = this.jAcc * dt_coef;
+      a.w -= j*a.i_inv;
+      b.w += j*b.i_inv;
+    },
+    
+    applyImpulse: function(){
+      var a = this.a;
+      var b = this.b;
+      
+      // compute relative rotational velocity
+      var wr = b.w - a.w + this.rate;
+      
+      // compute normal impulse
+      var j = -wr * this.iSum;
+      var jOld = this.jAcc;
+      this.jAcc = cpf.clamp( jOld + j, -this.jMax, this.jMax );
+      j = this.jAcc - jOld;
+      
+      // apply impulse
+      a.w -= j*a.i_inv;
+      b.w += j*b.i_inv;
+    },
+    
+    getImpulse: function(){
+      return Math.abs( this.jAcc );
+    }
+
+  });
+  
+  return SimpleMotor;
+});
