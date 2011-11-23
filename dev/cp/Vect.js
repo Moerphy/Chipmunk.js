c32330ce23d90a49d895464649d2f1d7a2d78322
Murphy <moerphy@googlemail.com>
2011-11-23 12:51:54 +0100

    Move package cp from dev/js/ to dev/
    Moved the package one level up, js dir was kinda unecessary.

--- dev/cp/Vect.js
+++ dev/cp/Vect.js
@@ -3,9 +3,9 @@ define([], function(){
 
   /**
    * Convenience constructor for cpVect structs
-   * @param x {Number} the x portion of the vector
-   * @param y {Number} the y portion of the vector
-   * @return {object} an object with the values as members (.x and .y)
+   * @param {Number} x the x portion of the vector
+   * @param {Number} y the y portion of the vector
+   * @return {cp/Vect} an object with the values as members (.x and .y)
    */
   var Vect = function(x, y){
     this.x = x;
@@ -15,7 +15,6 @@ define([], function(){
   Vect.prototype = {
     /**
      * Returns the length of v.
-     * @param v {object} the vector to use
      * @return {Number} the length of the vector.
      */
     length: function(){
@@ -25,9 +24,8 @@ define([], function(){
     
     /**
      * Linearly interpolate between v1 and v2.
-     * @param v1 {object} vector to interpolate from (source)
-     * @param v2 {object} vector to interpolate to (target)
-     * @param t {Number} the amount of v2 to use.
+     * @param {cp/Vect} v2 vector to interpolate to (target)
+     * @param {Number} the amount of v2 to use.
      */
     lerp: function( v2, t ){
       return this.mult(1 - t).add(v2.mult(t));
@@ -35,9 +33,8 @@ define([], function(){
      
     /**
      * Spherical linearly interpolate between v1 towards v2 by no more than angle a radians
-     * @param v1 {object} vector to interpolate from (source)
-     * @param v2 {object} vector to interpolate to (target)
-     * @param a {Number} angle of radians to use for interpolation.
+     * @param {cp/Vect} v2 vector to interpolate to (target)
+     * @param {Number} angle of radians to use for interpolation.
      */
     slerpconst: function( v2, a ){
       var angle = Math.acos(this.dot(v2)); 
@@ -47,8 +44,7 @@ define([], function(){
     
     /**
      * Returns the angular direction v is pointing in (in radians).
-     * @param v the vector to use.
-     * @return {object} Angular direction in radians.
+     * @return {Number} Angular direction in radians.
      */
     toangle: function(){
       return Math.atan2(this.y, this.x);
@@ -57,17 +53,15 @@ define([], function(){
     /**
      * Returns a string representation of v. Intended mostly for debugging purposes and not production use.
      * @attention The original C-function reset the local field on every call, this function does not.
-     * @param v {object} the vector to stringify
-     * @return {string} a string representation like "(1, 0)"
+     * @return {String} a string representation like "(1, 0)"
      */
     str: function(){
-      return "("+this.x+", "+this.y+")"; // TODO: find more elegant solution for this. Maybe grab a sprintf implementation for JS?
+      return "("+this.x+", "+this.y+")";
     },
 
     /**
      * Check if two vectors are equal. (Be careful when comparing floating point numbers!)
-     * @param v1 {object} the first vector
-     * @param v2 {object} the second vector
+     * @param {cp/Vect} v2 the second vector
      * @return {boolean}
      */
     eql: function(v2){
@@ -76,9 +70,8 @@ define([], function(){
 
     /**
      * Add two vectors
-     * @param v1 {object} the first vector
-     * @param v2 {object} the second vector
-     * @return {object} a new vector that is the sum of the two other vectors.
+     * @param {cp/Vect} v2 the second vector
+     * @return {cp/Vect} a new vector that is the sum of the two other vectors.
      */
     add: function(v2){
       return new Vect( this.x + v2.x, this.y + v2.y );
@@ -86,9 +79,8 @@ define([], function(){
     
     /**
      * Subtract two vectors.
-     * @param v1 {object} the first vector
-     * @param v2 {object} the second vector
-     * @return {object} a new vector that is the difference of the two other vectors.
+     * @param {cp/Vect} v2 the second vector
+     * @return {cp/Vect} a new vector that is the difference of the two other vectors.
      */
     sub: function(v2){
       return new Vect(this.x - v2.x, this.y - v2.y);
@@ -96,8 +88,7 @@ define([], function(){
 
     /**
      * Negate a vector
-     * @param v {object} the vector to negate
-     * @return {object} a vector pointing in the opposite direction ( -v.x, -v.y )
+     * @return {cp/Vect} a vector pointing in the opposite direction ( -v.x, -v.y )
      */
     neg: function(){
       return new Vect( -this.x, -this.y );
@@ -105,9 +96,8 @@ define([], function(){
     
     /**
      * Scalar multiplication
-     * @param v {object} the vector to multiplicate
      * @param s {Number} the scalar factor.
-     * @return {object} a new vector that is the scalar product
+     * @return {cp/Vect} a new vector that is the scalar product
      */
     mult: function(s){
       return new Vect( this.x*s, this.y*s );
@@ -115,8 +105,7 @@ define([], function(){
     
     /**
      * Vector dot product
-     * @param v1 {object} 
-     * @param v2 {object}
+     * @param {cp/Vect} v2
      * @return {Number}
      */
     dot: function(v2){
@@ -127,8 +116,7 @@ define([], function(){
      * 2D vector cross product analog.
      * The cross product of 2D vectors results in a 3D vector with only a z component.
      * This function returns the magnitude of the z value.
-     * @param v1 {object} 
-     * @param v2 {object}
+     * @param {cp/Vect} v2
      * @return {Number}
      */
     cross: function(v2){
@@ -137,8 +125,7 @@ define([], function(){
     
     /**
      * Returns a perpendicular vector. (90 degree rotation)
-     * @param v {object}
-     * @return {object}
+     * @return {cp/Vect}
      */
     perp: function(){
       return new Vect( -this.y, this.x );
@@ -146,28 +133,26 @@ define([], function(){
     
     /**
      * Returns a perpendicular vector. (-90 degree rotation)
-     * @param v {object}
-     * @return {object}
+     * @param {cp/Vect}
+     * @return {cp/Vect}
      */
     rperp: function(){
       return new Vect(this.y, -this.x);
     },
     
     /**
-     * Returns the vector projection of v1 onto v2.
-     * @param v1 {object}
-     * @param v2 {object}
-     * @return {object}
+     * Returns the vector projection of this onto v2.
+     * @param {cp/Vect} v2
+     * @return {cp/Vect}
      */
     project: function(v2){
       return v2.mult(this.dot(v2)/v2.dot(v2));
     },
     
     /**
-     * Uses complex number multiplication to rotate v1 by v2. Scaling will occur if v1 is not a unit vector.
-     * @param v1 {object}
-     * @param v2 {object}     
-     * @return {object}
+     * Uses complex number multiplication to rotate this by v2. Scaling will occur if v1 is not a unit vector.
+     * @param {cp/Vect} v2
+     * @return {cp/Vect}
      */
     rotate: function(v2){
       return new Vect(this.x*v2.x - this.y*v2.y, this.x*v2.y + this.y*v2.x);
@@ -175,17 +160,15 @@ define([], function(){
     
     /**
      * Inverse of rotate().
-     * @param v1 {object}
-     * @param v2 {object}     
-     * @return {object}
+     * @param {cp/Vect}  v2
+     * @return {cp/Vect}
      */
     unrotate: function(v2){
       return new Vect(this.x*v2.x + this.y*v2.y, this.y*v2.x - this.x*v2.y);
     },
     
     /**
-     * Returns the squared length of v. Faster than cpvlength() when you only need to compare lengths.
-     * @param v {object}
+     * Returns the squared length of this. Faster than cpvlength() when you only need to compare lengths.
      * @return {Number}
      */
     lengthsq: function(){
@@ -195,7 +178,7 @@ define([], function(){
     
     /**
      * Returns a normalized copy of v.
-     * @param v {object}
+     * @return {cp/Vect}
      */
     normalize: function(){
       return this.mult(1/this.length());
@@ -203,17 +186,15 @@ define([], function(){
     
     /**
      * Returns a normalized copy of v or cpvzero if v was already cpvzero. Protects against divide by zero errors.
-     * @param v {object}
-     * @return {object}
+     * @return {cp/Vect}
      */
     normalize_safe: function(){
       return (this.x === 0 && this.y === 0 ? Vect.zero : this.normalize());
     },
     
     /**
-     * Clamp v to length len
-     * @param v {object} 
-     * @param len {Number}
+     * Clamp this to length len
+     * @param {Number} len
      */
     clamp: function(len){
       return (this.dot(this) > len*len) ? this.normalize().mult(len) : this;
@@ -221,8 +202,7 @@ define([], function(){
     
     /**
      * Linearly interpolate between v1 towards v2 by distance d.
-     * @param v1 {object}
-     * @param v2 {object}
+     * @param {cp/Vect} v2
      * @param d {Number}
      */
     lerpconst: function(v2, d){
@@ -231,8 +211,7 @@ define([], function(){
     
     /**
      * Returns the distance between v1 and v2.
-     * @param v1 {object}
-     * @param v2 {object}
+     * @param {cp/Vect} v2
      * @return {Number}
      */
     dist: function(v1, v2){
@@ -241,8 +220,7 @@ define([], function(){
     
     /**
      * Returns the squared distance between v1 and v2. Faster than cpvdist() when you only need to compare distances.
-     * @param v1 {object}
-     * @param v2 {object}
+     * @param {cp/Vect} v2
      * @return {Number}
      */
     distsq: function(v2){
@@ -251,8 +229,7 @@ define([], function(){
     
     /**
      * Returns true if the distance between v1 and v2 is less than dist.
-     * @param v1 {object}
-     * @param v2 {object}
+     * @param {cp/Vect} v2
      * @return {boolean}
      */
     near: function(v2, dist){
@@ -260,9 +237,8 @@ define([], function(){
     },
     
     /**
-     * Spherical linearly interpolate between v1 and v2.
-     * @param v1 {object} vector to interpolate from (source)
-     * @param v2 {object} vector to interpolate to (target)
+     * Spherical linearly interpolate between v1 (this) and v2.
+     * @param {cp/Vect} v2 vector to interpolate to (target)
      * @param t {Number} 
      */
     cpvslerp: function(v2, t){
@@ -280,12 +256,15 @@ define([], function(){
       return '('+this.x + '/'+ this.y + ')';
     }
   };
-
+  
+  /**
+   * The (0,0) Vector
+   */
   Vect.zero = new Vect(0, 0);
   /**
    * Returns the unit length vector for the given angle (in radians).
-   * @param a {Number} Angle for the new vector in radians.
-   * @return {object} the unit length vector for this angle.
+   * @param {Number} a Angle for the new vector in radians.
+   * @return {cp/Vect} the unit length vector for this angle.
    */
   Vect.forangle = function(a){
     return new Vect(Math.cos(a), Math.sin(a));
